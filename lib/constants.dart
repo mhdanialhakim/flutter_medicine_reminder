44cf196dd406d41e909b033f94c1d33756e3c8af
DaHa <danialhakim028@gmail.com>
2023-06-08 00:31:54 +0800

    update

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color.fromARGB(255, 59, 117, 204);
+const Color kScaffoldColor = Color.fromARGB(255, 243, 238, 238);
+const Color kSecondaryColor = Color(0xFFF95C54);
+const Color kOtherColor = Color(0xFF59C1BD);
+const Color kErrorBorderColor = Color(0xFFE74C3C);
+const Color kTextLightColor = Color(0xFFC5BDCD);
+const Color kTextColor = Color.fromARGB(255, 74, 43, 89);
+const Color hTextColor = Color.fromARGB(255, 114, 56, 143);
